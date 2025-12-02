d8d023308c7373e8318f4d324ade51bf9b25549c
agrauss <antoniegrauss@hotmail.com>
2025-12-02 11:45:38 +0100

    Day1 part 1

--- 2025/day1/day1.cpp
+++ 2025/day1/day1.cpp
@@ -1,16 +1,63 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-#include <filesystem>
+#include <stdexcept>
 
 #include "read_file.hpp"
 
-int main() {
-    auto lines = AoC::utils::read_input_file("day1.txt");
+enum class Direction {
+    Left,
+    Right
+};
 
-    for (const std::string& line : lines) {
-        std::cout << line << std::endl;
+struct Instruction {
+    Direction direction;
+    int amount;
+
+    int apply(int original) const {
+        if (direction == Direction::Right) {
+            return (original + amount) % 100;
+        } else {
+            return (original - amount) % 100;
+        }
     }
+
+    static Instruction parse(const std::string& line) {
+        Instruction instruction;
+        if (line[0] == 'R') {
+            instruction.direction = Direction::Right;
+        } else if (line[0] == 'L') {
+            instruction.direction = Direction::Left;
+        } else {
+            throw std::invalid_argument("Line should start with R or L");
+        }
+
+        instruction.amount = std::stoi(line.substr(1, line.length() - 1));
+        return instruction;
+    }
+};
+
+int part1(std::string filename) {
+    auto lines = AoC::utils::read_input_file(filename);
+    std::vector<Instruction> instructions = {};
+    for (const std::string& line : lines) {
+        instructions.emplace_back(Instruction::parse(line));
+    }
+
+    // Counts how many times we stop on 0
+    int zero_counter = 0;
+    int dial_start = 50;
+    for (const Instruction& instruction : instructions) {
+        dial_start = instruction.apply(dial_start);
+        if (dial_start == 0) {
+            zero_counter++;
+        }
+    }
+
+    return zero_counter;
+}
+
+int main() {
+    int answer_part_1 = part1("day1.txt");
+    std::cout << "Answer of part 1: " << answer_part_1 << std::endl;
 }
