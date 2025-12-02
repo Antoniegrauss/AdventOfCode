f9d05523858edee0dca47efd2b5267dfdffd26e9
agrauss <antoniegrauss@hotmail.com>
2025-12-02 12:15:36 +0100

    Fix code duplication

--- 2025/day1/day1.cpp
+++ 2025/day1/day1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <functional>
 
 #include "read_file.hpp"
 
@@ -64,19 +65,22 @@ struct Instruction {
         instruction.amount = std::stoi(line.substr(1, line.length() - 1));
         return instruction;
     }
+
+    static std::vector<Instruction> parse_input_file(std::string filename) {
+        auto lines = AoC::utils::read_input_file(filename);
+        std::vector<Instruction> instructions = {};
+        for (const std::string& line : lines) {
+            instructions.emplace_back(Instruction::parse(line));
+        }
+        return instructions;
+    }
 };
 
 int part1(std::string filename) {
-    auto lines = AoC::utils::read_input_file(filename);
-    std::vector<Instruction> instructions = {};
-    for (const std::string& line : lines) {
-        instructions.emplace_back(Instruction::parse(line));
-    }
-
     // Counts how many times we stop on 0
     int zero_counter = 0;
     int dial_start = 50;
-    for (const Instruction& instruction : instructions) {
+    for (const Instruction& instruction : Instruction::parse_input_file(filename)) {
         dial_start = instruction.apply(dial_start);
         if (dial_start == 0) {
             zero_counter++;
@@ -88,26 +92,22 @@ int part1(std::string filename) {
 
 
 int part2(std::string filename) {
-    auto lines = AoC::utils::read_input_file(filename);
-    std::vector<Instruction> instructions = {};
-    for (const std::string& line : lines) {
-        instructions.emplace_back(Instruction::parse(line));
-    }
-
     // Counts how many times we stop on 0
     int zero_counter = 0;
     int dial_start = 50;
-    for (const Instruction& instruction : instructions) {
+    for (const Instruction& instruction : Instruction::parse_input_file(filename)) {
         zero_counter += instruction.count_zeroes(dial_start);
     }
 
     return zero_counter;
 }
 
-int main() {
-    int answer_part_1 = part1("day1.txt");
-    std::cout << "Answer of part 1: " << answer_part_1 << std::endl;
+void execute_part(std::function<int(std::string)> part_x, std::string file, int part_number) {
+    int answer_part_1 = part_x(file);
+    std::cout << "Answer of part " << part_number << ": " << answer_part_1 << std::endl;
+}
 
-    int answer_part_2 = part2("day1.txt");
-    std::cout << "Answer of part 1: " << answer_part_2 << std::endl;
+int main() {
+    execute_part(part1, "day1.txt", 1);
+    execute_part(part2, "day1.txt", 2);
 }
