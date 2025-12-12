d4c97f797ba2da607b00832ff5bbbf0512f97560
agrauss <antoniegrauss@hotmail.com>
2025-12-12 10:25:24 +0100

    Start day 12

---
+++ 2025/day12/day12.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <functional>
+#include <cctype>
+#include <set>
+#include <sstream>
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+
+#include "read_file.hpp"
+
+long part1(std::string filename)
+{
+    auto lines = AoC::utils::read_input_file(filename);
+    return 0;
+}
+
+long part2(std::string filename)
+{
+    auto lines = AoC::utils::read_input_file(filename);
+    return 0;
+}
+
+void execute_part(std::function<long(std::string)> part_x, std::string file, int part_number)
+{
+    long answer_part_1 = part_x(file);
+    std::cout << "Answer of part " << part_number << ": " << answer_part_1 << std::endl;
+}
+
+int main()
+{
+    // 500 is too high
+    execute_part(part1, "day12test.txt", 1);
+    execute_part(part2, "day12.txt", 2);
+    return 0;
+}
